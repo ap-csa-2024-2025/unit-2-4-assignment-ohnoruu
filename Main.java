f37e3c2b5cd5fa64c3792e356387d0520c9b2ec1
ohnoruu <rubydao007@gmail.com>
2024-10-03 11:22:30 -0700

    fref

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 public class Main
 {
-  public static void main(String[] args)
-  {
-    // write your code here
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    String string1 = sc.nextLine();
+    String string2 = sc.nextLine();
+    int num = sc.nextInt();
+    System.out.println(lastFirstN(string1, string2, num));
   }
+
+  public static String lastFirstN(String s1, String s2, int n){
+    String lastN = s1.substring(n-1);
+    String firstN = s2.substring(0,n);
+    return(lastN+firstN);
+  }
+  /* 
+  public static void stringManip(String s1, String s2){
+    String upS1 = s1.toUpperCase();
+    String firstLetterS2 = s2.substring(0,1);
+  }
+  */
 }
---
+++ Notes.java
@@ -0,0 +1,14 @@
+public class Notes {
+    public static void main(String[] args)
+  {
+    String mySchool = "WHS";
+    int myGrade = 11;
+    double myGPA = 4.6;
+    boolean isGoingHoco = false;
+    
+    int length = 3;
+    int width = 5;
+    System.out.println("Perimeter: " + (2*length + 2*width));
+    System.out.println("Area: " + (length * width));
+  }
+}
