012d7a934fb7fc672ea3e6f4dc38cdb83bdb098c
ohnoruu <rubydao007@gmail.com>
2024-10-04 11:16:39 -0700

    yippee

--- Main.java
+++ Main.java
@@ -3,21 +3,33 @@ public class Main
 {
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
+    
     String string1 = sc.nextLine();
     String string2 = sc.nextLine();
     int num = sc.nextInt();
     System.out.println(lastFirstN(string1, string2, num));
+    sc.nextLine();
+    String stringA = sc.nextLine();
+    String stringB = sc.nextLine();
+    stringManip(stringA, stringB);
   }
 
   public static String lastFirstN(String s1, String s2, int n){
-    String lastN = s1.substring(n-1);
-    String firstN = s2.substring(0,n);
-    return(lastN+firstN);
+    int indexOfNToLast = s1.length() - n;
+    String lastS1 = s1.substring(indexOfNToLast);
+    String firstS2 = s2.substring(0,n);
+    String output = lastS1 + firstS2;
+    return output;
   }
-  /* 
+  
   public static void stringManip(String s1, String s2){
     String upS1 = s1.toUpperCase();
     String firstLetterS2 = s2.substring(0,1);
+    String upperLetterS2 = firstLetterS2.toUpperCase();
+    String lettersS2 = s2.substring(1);
+    String lowerLettersS2 = lettersS2.toLowerCase();
+    System.out.println(upS1);
+    System.out.println(upperLetterS2 + lowerLettersS2);
   }
-  */
+  
 }
